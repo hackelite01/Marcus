1411a0aec2528aa56a845bfe60f1a2bc39a83c51
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-15 10:04:23 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/HACKERBOTTELEGRAM/HACKERBOTOP && cd HACKERBOTOP && python3 -m userbot")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m ULTRA")
+system("python3 -m userbot")
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
