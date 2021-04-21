bd56bbd2133e175d3d53882ee42e626b979a18df
hackelite01 <78653024+hackelite01@users.noreply.github.com>
2021-04-21 11:17:41 +0530

    Update start.py

--- start.py
+++ start.py
@@ -3,4 +3,4 @@ system ("git clone https://GitHub.com/hackelite01/Marcususerbot && cd Marcususer
 # try af
 print("OH SYSTEM IS CRASHED")
 system("python3 -m userbot")
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @DARKLON_USERBOT_SUPPORT")
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @hackelitebotlist")
