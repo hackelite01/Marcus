45b12e4620c9de7452bbdea7809dce69fa988796
HackElite <78653024+hackelite01@users.noreply.github.com>
2021-06-11 11:42:14 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from os import system # chala ja 
-system ("git clone https://GitHub.com/hackelite01/Marcususerbot && cd Marcususerbot && python3 -m MARCUS")
+system ("git clone -b new https://GitHub.com/hackelite01/Marcususerbot && cd Marcususerbot && python3 -m MARCUS")
 # try af
-print("OH SYSTEM IS CRASHED")
-system("python3 -m MARCUS")
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @hackelitebotlist")
+
+
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @MarcusUserbot")
